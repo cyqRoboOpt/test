c0916d074153ffa7e9e1105cbd457ad576b10776
cyqRoboOpt <143577845+cyqRoboOpt@users.noreply.github.com>
2023-08-31 12:08:11 +0800

    add plot
    add command to plot sin wave

--- test.m
+++ test.m
@@ -1,6 +1,14 @@
+clc,clear
+close all
+
 H = [1 -1; -1 2]; 
 f = [-2; -6];
 A = [1 1; -1 2; 2 1];
 b = [2; 2; 3];
 
 [x,fval,exitflag,output,lambda] = quadprog(H,f,A,b);
+
+%%
+t=0:0.01:6;
+y=sin(t);
+plot(t,y)
